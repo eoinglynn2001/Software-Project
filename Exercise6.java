7b44b48f417e5bcf891c1d487f874e9232feeb3f
eoinglynn2001 <55384722+eoinglynn2001@users.noreply.github.com>
2020-05-01 20:58:36 +0100

    Exercise 6

---
+++ Exercise6.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+public class Exercise6{
+         public static void main(String[] args){
+
+
+			 String symbol;
+			 int rows = 1;
+			 int columns = 1;
+			 int no_of_rows = 0;
+	         int no_of_columns = 0;
+	         Scanner input = new Scanner(System.in);
+
+			System.out.print("Enter a symbol: ");
+			 symbol = input.next();
+
+			 System.out.print("Enter number of rows: ");
+			 no_of_rows = input.nextInt();
+
+			 System.out.print("Enter number of colunms: ");
+			 no_of_columns = input.nextInt();
+
+     		while(rows<= no_of_rows){
+		    while(columns<= no_of_columns){
+
+
+
+
+	 System.out.print(symbol);
+	  columns++;
+}
+
+	 columns = 1;
+	 System.out.println(
+		 );
+	 rows++;
+}
+
+
+
+
+
+	}
+}
