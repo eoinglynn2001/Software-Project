466731d119a6e1922714921eeef6013f048eab13
eoinglynn2001 <55384722+eoinglynn2001@users.noreply.github.com>
2020-05-01 17:26:15 +0100

    Exercise 1

---
+++ Exercise1.java
@@ -0,0 +1,10 @@
+public class Exercise1{
+        public static void main(String[] args) {
+            double weight = 65.48;
+            int age = 20;
+            String color = "Red";
+
+             System.out.println("Mary is "+age+" years old.\nShe weighs "+weight+" kilograms.\nHer favourite color is "+color+".");
+
+        }
+}
